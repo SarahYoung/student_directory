56c4cecbefe6df0432d31fe606b2410ea19ce16e
Sarah Young <y.sarahc@gmail.com>
2014-02-28 13:52:41 +0000

    Put student names into an array

--- directory.rb
+++ directory.rb
@@ -1,32 +1,56 @@
 student_count = 23
 
+students = [
+ "Mario Gintili",
+ "Rick Brunstedt",
+ "Mikhail Dubov",
+ "Karolis Noreika",
+ "Michael Sidon",
+ "Charles de Barros",
+ "Ruslan Vikhor",
+ "Toby Retallick",
+ "Mark Mekhail",
+ "Sarah Young",
+ "Hanna Wight",
+ "Khushkaren Singh",
+ "Manjit Singh",
+ "Alex Gaudiosi",
+ "Ross Hepburn",
+ "Natascia Marchese",
+ "Tiffanie Chia",
+ "Matthew Thomas",
+ "Freddy McGroarty",
+ "Tyler Rollins",
+ "Richard Curteis",
+ "Anna Yanova",
+ "Andrew Cumine",
+]
+
 puts "The students of my cohort at Makers Academy"
 puts "-----------------"
 
-puts "Mario Gintili"
-puts "Rick Brunstedt"
-puts "Mikhail Dubov"
-puts "Karolis Noreika"
-puts "Michael Sidon"
-puts "Charles de Barros"
-puts "Ruslan Vikhor"
-puts "Toby Retallick"
-puts "Mark Mekhail"
-puts "Sarah Young"
-puts "Hanna Wight"
-puts "Khushkaren Singh"
-puts "Manjit Singh"
-puts "Alex Gaudiosi"
-puts "Ross Hepburn"
-puts "Natascia Marchese"
-puts "Tiffanie Chia"
-puts "Matthew Thomas"
-puts "Freddy McGroarty"
-puts "Tyler Rollins"
-puts "Richard Curteis"
-puts "Anna Yanova"
-puts "Andrew Cumine"
+puts students [1]
+puts students [2]
+puts students [3]
+puts students [4]
+puts students [5]
+puts students [6]
+puts students [7]
+puts students [8]
+puts students [9]
+puts students [10]
+puts students [11]
+puts students [12]
+puts students [13]
+puts students [14]
+puts students [15]
+puts students [16]
+puts students [17]
+puts students [18]
+puts students [19]
+puts students [20]
+puts students [21]
+puts students [22]
+puts students [23]
 
-print "Overall, we have "
-print student_count
-print " great students"
+print "Overall, we have #{student_count} great students"
