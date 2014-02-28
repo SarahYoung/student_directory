08eff04e67d8287f1bcf1d5f82558272561d134e
Sarah Young <y.sarahc@gmail.com>
2014-02-28 14:50:51 +0000

    Added input_students method deleted students hash

--- directory.rb
+++ directory.rb
@@ -1,30 +1,15 @@
-student_count = 23
-
-students = [
- {:name => "Mario Gintili", :cohort => :February},
- {:name => "Rick Brunstedt", :cohort => :February},
- {:name => "Mikhail Dubov", :cohort => :February},
- {:name => "Karolis Noreika", :cohort => :February},
- {:name => "Michael Sidon", :cohort => :February},
- {:name => "Charles de Barros", :cohort => :February},
- {:name => "Ruslan Vikhor", :cohort => :February},
- {:name => "Toby Retallick", :cohort => :February},
- {:name => "Mark Mekhail", :cohort => :February},
- {:name => "Sarah Young", :cohort => :February},
- {:name => "Hanna Wight", :cohort => :February},
- {:name => "Khushkaren Singh", :cohort => :February},
- {:name => "Manjit Singh", :cohort => :February},
- {:name => "Alex Gaudiosi", :cohort => :February},
- {:name => "Ross Hepburn", :cohort => :February},
- {:name => "Natascia Marchese", :cohort => :February},
- {:name => "Tiffanie Chia", :cohort => :February},
- {:name => "Matthew Thomas", :cohort => :February},
- {:name => "Freddy McGroarty", :cohort => :February},
- {:name => "Tyler Rollins", :cohort => :February},
- {:name => "Richard Curteis", :cohort => :February},
- {:name => "Anna Yanova", :cohort => :February},
- {:name => "Andrew Cumine",:cohort =>  :February}
-]
+def input_students
+	puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+		students = []
+		name = gets.chomp
+		while !name.empty? do
+			students << {:name => name, :cohort => :February}
+			puts "Now we have #{students.length} students"
+			name = gets.chomp
+		end
+		students
+end
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -41,6 +26,7 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
