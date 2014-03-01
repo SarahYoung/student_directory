4c783ee73606510efad4a981e1568e13ae036647
Sarah Young <y.sarahc@gmail.com>
2014-03-01 17:51:22 +0000

    Exercise 7: added .center() method to change display

--- directory.rb
+++ directory.rb
@@ -1,33 +1,33 @@
 students = [
- {:name => "Mario Gintili", :cohort => :February},
- {:name => "Rick Brunstedt", :cohort => :February},
- {:name => "Mikhail Dubov", :cohort => :February},
- {:name => "Karolis Noreika", :cohort => :February},
- {:name => "Michael Sidon", :cohort => :February},
- {:name => "Charles de Barros", :cohort => :February},
- {:name => "Ruslan Vikhor", :cohort => :February},
- {:name => "Toby Retallick", :cohort => :February},
- {:name => "Mark Mekhail", :cohort => :February},
- {:name => "Sarah Young", :cohort => :February},
- {:name => "Hanna Wight", :cohort => :February},
- {:name => "Khushkaren Singh", :cohort => :February},
- {:name => "Manjit Singh", :cohort => :February},
- {:name => "Alex Gaudiosi", :cohort => :February},
- {:name => "Ross Hepburn", :cohort => :February},
- {:name => "Natascia Marchese", :cohort => :February},
- {:name => "Tiffanie Chia", :cohort => :February},
- {:name => "Matthew Thomas", :cohort => :February},
- {:name => "Freddy McGroarty", :cohort => :February},
- {:name => "Tyler Rollins", :cohort => :February},
- {:name => "Richard Curteis", :cohort => :February},
- {:name => "Anna Yanova", :cohort => :February},
- {:name => "Andrew Cumine",:cohort =>  :February}
+ {:name => "Mario Gintili", :cohort => :February, :hobby => "coding"},
+ {:name => "Rick Brunstedt", :cohort => :February, :hobby => "coding"},
+ {:name => "Mikhail Dubov", :cohort => :February, :hobby => "coding"},
+ {:name => "Karolis Noreika", :cohort => :February, :hobby => "coding"},
+ {:name => "Michael Sidon", :cohort => :February, :hobby => "coding"},
+ {:name => "Charles de Barros", :cohort => :February, :hobby => "coding"},
+ {:name => "Ruslan Vikhor", :cohort => :February, :hobby => "coding"},
+ {:name => "Toby Retallick", :cohort => :February, :hobby => "coding"},
+ {:name => "Mark Mekhail", :cohort => :February, :hobby => "coding"},
+ {:name => "Sarah Young", :cohort => :February, :hobby => "coding"},
+ {:name => "Hanna Wight", :cohort => :February, :hobby => "coding"},
+ {:name => "Khushkaren Singh", :cohort => :February, :hobby => "coding"},
+ {:name => "Manjit Singh", :cohort => :February, :hobby => "coding"},
+ {:name => "Alex Gaudiosi", :cohort => :February, :hobby => "coding"},
+ {:name => "Ross Hepburn", :cohort => :February, :hobby => "coding"},
+ {:name => "Natascia Marchese", :cohort => :February, :hobby => "coding"},
+ {:name => "Tiffanie Chia", :cohort => :February, :hobby => "coding"},
+ {:name => "Matthew Thomas", :cohort => :February, :hobby => "coding"},
+ {:name => "Freddy McGroarty", :cohort => :February, :hobby => "coding"},
+ {:name => "Tyler Rollins", :cohort => :February, :hobby => "coding"},
+ {:name => "Richard Curteis", :cohort => :February, :hobby => "coding"},
+ {:name => "Anna Yanova", :cohort => :February, :hobby => "coding"},
+ {:name => "Andrew Cumine",:cohort => :February, :hobby => "coding"}
 ]
 
 
 def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-----------------"
+	puts "The students of my cohort at Makers Academy".center(100)
+	puts "-----------------".center(100)
 end
 
 def print(students)
@@ -35,13 +35,13 @@ def print(students)
 	student = {}
 	while i < students.length
 		student = students[i]
-		puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+		puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort), hobby: #{student[:hobby]}".center(100)
 		i += 1
 	end
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+	puts "Overall, we have #{names.length} great students".center(100)
 end
 
 print_header
